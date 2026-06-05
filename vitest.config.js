a0ae30bb51e6f33b8500270340376150fd5fa1b7
scmlearningcenter <scmlearningcenter@gmail.com>
2026-06-05 12:26:33 +0530

    update code

--- vitest.config.js
+++ vitest.config.js
@@ -2,6 +2,11 @@ import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
     test: {
-        environment: 'jsdom'
+        environment: 'jsdom',
+        coverage: {
+            provider: 'v8',
+            reporter: ['text', 'lcov'],
+            reportsDirectory: './coverage'
+        }
     }
 })
